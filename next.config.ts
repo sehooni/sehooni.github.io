ff69b52296cfe8bb954b5421235528ff87b2cc94
SEHOON PARK <74sehoon@gmail.com>
2025-12-15 15:21:27 +0900

    fix: Next.js security problem

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-import path from 'path';
+
 
 const nextConfig: NextConfig = {
   output: 'export',
@@ -8,13 +8,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.join(process.cwd()),
-    };
-    return config;
-  },
+
 };
 
 export default nextConfig;
