48424f3322965586bef88d341196dc3836ba5570
SEHOON PARK <74sehoon@gmail.com>
2025-12-23 12:57:34 +0900

    feat: update the remume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -7,7 +7,7 @@ export default function Resume() {
             <main className="flex-1 w-full max-w-screen-2xl mx-auto p-4 md:p-8">
                 <div className="w-full h-[calc(100vh-200px)] min-h-[800px]">
                     <iframe
-                        src="https://drive.google.com/file/d/182BKpahYqTzXWf8sY1Z_XA64vx8guOzA/preview"
+                        src="https://drive.google.com/file/d/1iMoZotUYJK73VZLQBYTMR8XH4g_UqQsh/preview"
                         className="w-full h-full border-none rounded-lg shadow-lg"
                         allow="autoplay"
                     ></iframe>
