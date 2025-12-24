5effc2862e580f26d320803f6bea4251d75fde14
SEHOON PARK <74sehoon@gmail.com>
2025-12-24 11:22:42 +0900

    feat: update the resume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -7,7 +7,7 @@ export default function Resume() {
             <main className="flex-1 w-full max-w-screen-2xl mx-auto p-4 md:p-8">
                 <div className="w-full h-[calc(100vh-200px)] min-h-[800px]">
                     <iframe
-                        src="https://drive.google.com/file/d/1iMoZotUYJK73VZLQBYTMR8XH4g_UqQsh/preview"
+                        src="https://drive.google.com/file/d/15THPIGCw65B2eOMRQuQvzoOb4fqbNLKc/preview"
                         className="w-full h-full border-none rounded-lg shadow-lg"
                         allow="autoplay"
                     ></iframe>
