f5b8ad02f550597665bd94470650807bc8f8f3bf
Alpha-Agent <74sehoon@gmail.com>
2026-03-09 21:20:16 +0900

    docs: update embedded resume PDF link.

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -4,7 +4,7 @@ export default function Resume() {
             <main className="flex-1 w-full max-w-screen-2xl mx-auto p-4 md:p-8">
                 <div className="w-full h-[calc(100vh-200px)] min-h-[800px]">
                     <iframe
-                        src="https://drive.google.com/file/d/15THPIGCw65B2eOMRQuQvzoOb4fqbNLKc/preview"
+                        src="https://drive.google.com/file/d/1QAMvX35qfbErZGaPdtQA75NyiUh555KU/preview"
                         className="w-full h-full border-none rounded-lg shadow-lg"
                         allow="autoplay"
                     ></iframe>
